38abaf8c46ddbaee326c98fceaa62310c5f999ec
ercanalayli <ercanalayli@gmail.com>
2026-07-07 10:45:45 +0300

    Add Gorukle Medical additional order data

---
+++ hasta-bezi/gorukle_ilave.js
@@ -0,0 +1,15 @@
+window.HASTA_BEZI_GORUKLE_ILAVE = {
+  updateNo: '1043260707',
+  customer: 'GÖRÜKLE MEDİKAL / BURSA',
+  note: 'Görükle Medikal ilave: 10 balya Moly serme. Sermeler Nisan listesi, diğerleri Mayıs listesi.',
+  replaceOrderName: 'GÖRÜKLE MEDİKAL / BURSA',
+  replaceProductsFor: 'GÖRÜKLE',
+  order: ['GÖRÜKLE MEDİKAL / BURSA','02.07.2026 + 07.07.2026 Moly ilave','FATURA BEKLİYOR','SEVK EDİLMEDİ','Sermeler Nisan / Diğerleri Mayıs','Yok','Bursa / Orhangazi',31416.08,34994.66,21867.71,1250,8298.37],
+  products: [
+    ['GÖRÜKLE','Coverdry serme 60x90',60,'Nisan / 18.04.2025','Yok',80.66,'30.11.2025','IST2025000001583','Uygulanmaz',141.53,7719.82,2880.52],
+    ['GÖRÜKLE','Jender 90x180 serme',30,'Nisan / 18.04.2025','Yok',187.34,'30.04.2026','IST2026000000285','M012026000000113',234.718,6401.40,781.20],
+    ['GÖRÜKLE','Moly serme 60x90',60,'Nisan / 18.04.2025','Yok',71.25,'30.11.2025','IST2025000001583','Uygulanmaz',150.00,8181.82,3906.82],
+    ['GÖRÜKLE','Lorina mesane pedi',12,'Mayıs 2026','Yok',300.39,'31.07.2025','PDE2025000001241','Kontrol paneli',434.808,4743.36,1138.68],
+    ['GÖRÜKLE','Lorina çinko krem',12,'Mayıs 2026','Yok',294.04,'31.01.2026','IST2026000000080','M012026000000046',436.968,4369.68,841.15]
+  ]
+};
