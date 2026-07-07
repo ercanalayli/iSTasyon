0ca03f5e2ae64aba41d1bbbd9cb5d66631706542
ercanalayli <ercanalayli@gmail.com>
2026-07-07 12:30:33 +0300

    Add issued invoice proof for Zeybek and Sercan orders

---
+++ hasta-bezi/faturalar_03072026.js
@@ -0,0 +1,31 @@
+window.HASTA_BEZI_FATURA_GUNCELLEME = {
+  updateNo: '1105260707',
+  invoices: [
+    {
+      customerKey: 'ZEYBEK',
+      orderCustomer: 'ZEYBEK MEDİKAL / KÜTAHYA',
+      invoiceNo: 'M012026000000156',
+      invoiceDate: '03.07.2026',
+      status: 'FATURA KESİLDİ',
+      shipmentStatus: 'SEVK TARİHİ BEKLİYOR',
+      net: 129228.74,
+      vat: 12922.87,
+      gross: 142151.59,
+      ettn: 'd2ae1b31-d881-4395-8aff-601ac395361f',
+      note: 'Zeybek faturası yüklendi; sevk tarihi olmadan sipariş kapanmaz.'
+    },
+    {
+      customerKey: 'SERCAN',
+      orderCustomer: 'SERCAN MEDİKAL / ESKİŞEHİR',
+      invoiceNo: 'M012026000000155',
+      invoiceDate: '03.07.2026',
+      status: 'FATURA KESİLDİ',
+      shipmentStatus: 'SEVK TARİHİ BEKLİYOR',
+      net: 28770.79,
+      vat: 2877.08,
+      gross: 31647.88,
+      ettn: '694ba9e0-45db-4569-9da8-957e6f8b3199',
+      note: 'Sercan faturası yüklendi; sevk tarihi olmadan sipariş kapanmaz.'
+    }
+  ]
+};
