a1817746ec30d7215f6d22c6388c0b26b057bb18
ercanalayli <ercanalayli@gmail.com>
2026-07-26 16:20:02 +0300

    AperiON tek merkez bağlantısını tamamla

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aperion-mobile-v120';
-const SHELL=['/aperion-home-v3.html','/manifest.json','/aperion-mobile.css','/aperion-mobile.js','/aperion-icon-192-v2.png','/aperion-icon-512-v2.png'];
+const CACHE='aperion-mobile-v121';
+const SHELL=['/aperion-merkez.html','/aperion-home-v3.html','/manifest.json','/aperion-mobile.css','/aperion-mobile.js','/aperion-icon-192-v2.png','/aperion-icon-512-v2.png','/data/aperion_surface_inventory.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('/aperion-home-v3.html'))))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();event.waitUntil(caches.open(CACHE).then(cache=>cache.put(event.request,copy)));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('/aperion-merkez.html'))))});
